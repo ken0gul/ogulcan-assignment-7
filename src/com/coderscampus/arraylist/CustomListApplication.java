37fa5a4487fc371de8af3d8bf46168e7e15da002
TheJohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-10-18 17:39:18 -0400

    Delete unused codes

--- src/com/coderscampus/arraylist/CustomListApplication.java
+++ src/com/coderscampus/arraylist/CustomListApplication.java
@@ -7,33 +7,13 @@ public class CustomListApplication {
 
 	public static void main(String[] args) {
 		CustomList<String> yourList = new CustomArrayList<>();
-		
-		yourList.add("John");
+
 		yourList.add("Sam");
 		yourList.add("Maggie");
-		yourList.add("Tom");
-		yourList.add("Tony");
-		yourList.add("Margaret");
-		yourList.add("John");
-		yourList.add("Sam");
-		yourList.add("Maggie");
-		yourList.add("John");
-		yourList.add("Sam");
-		yourList.add("Maggie");
-		yourList.add("John");
-		yourList.add("Sam");
-		yourList.add("Maggie");
-		yourList.add("John");
-		yourList.add("Sam");
-		yourList.add("Maggie");
-		yourList.add("John");
-		yourList.add("Sam");
-		yourList.add("Maggie");
-		yourList.add("John");
-		yourList.add("Sam");
-		yourList.add("Maggie");
+		yourList.add(0, "Kebo");
+		yourList.remove(0);
 		System.out.println(yourList);
-		System.out.println(yourList.get(5));
+		System.out.println(yourList.get(0));
 		System.out.println(yourList.getSize());
 
 	}
